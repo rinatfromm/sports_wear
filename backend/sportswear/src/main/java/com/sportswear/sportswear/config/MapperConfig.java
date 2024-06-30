5baf0d64a1155b6193f81d045c7dc89d54938cce
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-06-30 22:57:30 +0200

    add banner, log config, app properties, mapper config. Base dependencies.

---
+++ backend/sportswear/src/main/java/com/sportswear/sportswear/config/MapperConfig.java
@@ -0,0 +1,19 @@
+package com.sportswear.sportswear.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE);
+        return mapper;
+    }
+}
