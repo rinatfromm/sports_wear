1a892bb25139306e7e5730a001c1a376996e2693
Vladyslav Kogutiak <vlad77solo@gmail.com>
2024-06-30 22:24:11 +0200

    creating base for sportswear shop

---
+++ backend/sportswear/src/main/java/com/sportswear/sportswear/SportswearApplication.java
@@ -0,0 +1,13 @@
+package com.sportswear.sportswear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportswearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportswearApplication.class, args);
+	}
+
+}
---
+++ backend/sportswear/src/test/java/com/sportswear/sportswear/SportswearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportswear.sportswear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportswearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
